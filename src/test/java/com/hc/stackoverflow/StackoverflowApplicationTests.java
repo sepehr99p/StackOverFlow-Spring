21c42fd5d9a7c5aafc40f003776fe9890055278d
Sep <sepehr.pourekrami@gmail.com>
2025-04-14 12:09:46 +0330

    init project

---
+++ src/test/java/com/hc/stackoverflow/StackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
