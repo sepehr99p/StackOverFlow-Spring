a886d4538eddc7282e99b66938bdb5e33f7e4189
Sep <sepehr.pourekrami@gmail.com>
2025-04-15 17:47:02 +0330

    fix lombok dependency

--- src/main/java/com/hc/stackoverflow/entity/dto/ErrorResponse.java
+++ src/main/java/com/hc/stackoverflow/entity/dto/ErrorResponse.java
@@ -5,17 +5,12 @@ import lombok.*;
 
 import java.util.Map;
 
+@Setter
+@Getter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
-    public Map<String, String> getErrorMessage() {
-        return errorMessage;
-    }
-
-    public void setErrorMessage(Map<String, String> errorMessage) {
-        this.errorMessage = errorMessage;
-    }
 
     private Map<String,String> errorMessage;
 }
